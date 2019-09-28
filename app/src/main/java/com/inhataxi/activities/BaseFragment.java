f68a3d394e3924adcd779b244ffc6ee949793812
jangsm <tmdalsy@naver.com>
2019-09-28 22:48:14 +0900

    커밋 1차

---
+++ app/src/main/java/com/inhataxi/activities/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.inhataxi.activities;
+
+import android.view.View;
+
+import androidx.fragment.app.Fragment;
+
+public abstract class BaseFragment extends Fragment {
+
+    public abstract void setComponentView(View v);
+}
