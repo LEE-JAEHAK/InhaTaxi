2210c688ebe5123851eacbcae9f00ff92f04a4fc
jangsm <tmdalsy@naver.com>
2019-09-29 12:30:35 +0900

    커밋 최종

--- app/src/main/java/com/inhataxi/activities/ChatActivity.java
+++ app/src/main/java/com/inhataxi/activities/ChatActivity.java
@@ -3,6 +3,7 @@ package com.inhataxi.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.util.Log;
@@ -37,6 +38,12 @@ public class ChatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View view = getWindow().getDecorView();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (view != null) {
+                view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            }
+        }
         setContentView(R.layout.activity_chat);
 
         // 위젯 ID 참조
